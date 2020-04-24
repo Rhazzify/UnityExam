19b5e109ce996bc651c323a1c0e2304ffab7d96a
Kittikorn.D <Kittikorn-detnoi@hotmail.com>
2020-04-24 22:36:17 +0200

    Almost thereeee

---
+++ UnityExam2020/Assets/Scripts/KeypadPanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeypadPanel : MonoBehaviour
+{
+    [SerializeField]
+    Text KeypadText;
+
+    string KeypadValue = "";
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeypadText.text = KeypadValue;
+
+        if (KeypadValue == "1111")
+        {
+            TriggerEnterKeypad.DoorIsOpen = true;
+        }
+
+
+
+        if (KeypadValue.Length >= 4)
+        {
+            KeypadValue = "";
+        }
+
+    }
+    public void AddDigit(string digit)
+    {
+        KeypadValue += digit;
+    }
+}
---
+++ UnityExam2020/Assets/Scripts/TriggerLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLight : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject Prosjektor, Clue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Clue.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Clue.SetActive(false);
+    }
+}
