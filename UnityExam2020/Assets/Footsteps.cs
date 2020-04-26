53064d42c5a215ba3503b87ee518e342ba68187f
knurk <knuheg@hotmail.com>
2020-04-26 20:16:16 +0200

    Topp

---
+++ UnityExam2020/Assets/Footsteps.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    CharacterController cc;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
